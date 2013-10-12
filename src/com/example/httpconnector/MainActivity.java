79a6c63c6acff6cba229eb158bc1fe3179cbc8c4
Sudipto Aich <saich@ford.com>
2013-10-11 20:45:26 -0700

    Got the HTTP post executed

--- src/com/example/httpconnector/MainActivity.java
+++ src/com/example/httpconnector/MainActivity.java
@@ -3,18 +3,31 @@
 
 package com.example.httpconnector;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -66,21 +79,78 @@ public class MainActivity extends Activity implements OnClickListener{
     	switch(view.getId()){
     	case R.id.button1:
     	//do stuff
+    	Log.i ("GetButtonPressed","Success");
     	GetXMLTask task = new GetXMLTask();
         task.execute(new String[] { URL });
     	break;
     	
     	case R.id.button2:
     	//do stuff
-    	//println (URL);	
+    	Log.i ("PostButtonPressed","Success");
+    	postData();	
     	break;
     	}
-    	GetXMLTask task = new GetXMLTask();
-         
-        task.execute(new String[] { URL });
+    	//GetXMLTask task = new GetXMLTask();
+        //task.execute(new String[] { URL });
     }
  
-    private class GetXMLTask extends AsyncTask<String, Void, String> {
+    private void postData() {
+		// TODO Auto-generated method stub
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        //HttpPost httppost = new HttpPost("http://192.168.2.126:8000");
+        HttpPost httppost = new HttpPost(URL);
+        Log.i("success","executed postData");
+        
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+            nameValuePairs.add(new BasicNameValuePair("stringdata", "test data to be posted"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            //HttpResponse httppost = httpclient.execute((HttpUriRequest) httppost);
+            Log.w("Success", "Execute HTTP Post Request");     
+            HttpResponse response = httpclient.execute(httppost);
+            
+            String str = inputStreamToString(response.getEntity().getContent()).toString();
+            Log.w("Success", str);
+//            
+//            if(str.toString().equalsIgnoreCase("true"))
+//            {
+//             Log.w("Success", "TRUE");
+//             outputText.setText("Login successful");   
+//            }else
+//            {
+//             Log.w("Success", "FALSE");
+//             outputText.setText(str);             
+//            }
+ 
+        } catch (ClientProtocolException e) {
+         e.printStackTrace();
+        } catch (IOException e) {
+         e.printStackTrace();
+        }
+	}
+    private StringBuilder inputStreamToString(InputStream is) {
+        String line = "";
+        StringBuilder total = new StringBuilder();
+        // Wrap a BufferedReader around the InputStream
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        // Read response until the end
+        try {
+         while ((line = rd.readLine()) != null) { 
+           total.append(line); 
+         }
+        } catch (IOException e) {
+         e.printStackTrace();
+        }
+        // Return full string
+        return total;
+       }
+
+	private class GetXMLTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
             String output = null;
@@ -115,5 +185,30 @@ public class MainActivity extends Activity implements OnClickListener{
         protected void onPostExecute(String output) {
             outputText.setText(output);
         }
+    
+    // Post block before merge
+        // Http post request
+/*		public void postData() {
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://192.168.2.126:8000");
+
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+                nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+                
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            }
+        } */
+    
     }
 }
